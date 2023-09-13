9476bb48268879917ffad32734373266fdafbebc
aya-yay <yabuuchi.a.yano@gmail.com>
2023-09-13 13:36:21 +0900

    deploy

--- app.js
+++ app.js
@@ -28,49 +28,49 @@ start();
 //contact-form server.js
 //Middleware
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '../public/index.html');
-})
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '../public/index.html');
+// })
 
-app.post('/', (req, res) => {
-  console.log(req.body);
+// app.post('/', (req, res) => {
+//   console.log(req.body);
 
-  const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 587,
-    auth: {
-      user: process.env.GMAILUSER,
-      pass: process.env.GMAILPASSWORD,
-    },
-  });
+//   const transporter = nodemailer.createTransport({
+//     host: 'smtp.gmail.com',
+//     port: 587,
+//     auth: {
+//       user: process.env.GMAILUSER,
+//       pass: process.env.GMAILPASSWORD,
+//     },
+//   });
   //管理人が受け取るメール
-  const mailOptions = {
-    from: req.body.email,
-    to: 'yabuuchi.a.yano@gmail.com',
-    subject: `[お問い合わせ]: ${req.body.name}様より`,
-    text: `${req.body.message} Send from ${req.body.email}`,
-    html: `
-        <p>【名前】</p>
-        <p>${req.body.name}</p>
+//   const mailOptions = {
+//     from: req.body.email,
+//     to: 'yabuuchi.a.yano@gmail.com',
+//     subject: `[お問い合わせ]: ${req.body.name}様より`,
+//     text: `${req.body.message} Send from ${req.body.email}`,
+//     html: `
+//         <p>【名前】</p>
+//         <p>${req.body.name}</p>
 
-        <p>【メールアドレス】</p>
-        <p>${req.body.email}</p>
+//         <p>【メールアドレス】</p>
+//         <p>${req.body.email}</p>
 
-        <p>【メッセージ内容】</p>
-        <p>${req.body.message}</p>
-        `,
-  };
+//         <p>【メッセージ内容】</p>
+//         <p>${req.body.message}</p>
+//         `,
+//   };
 
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.log(error);
-      res.send('error');
-    } else {
-      console.log("送信完了: " + info.response);
-      res.send("成功しました");
-    }
-  })
-})
+//   transporter.sendMail(mailOptions, (error, info) => {
+//     if (error) {
+//       console.log(error);
+//       res.send('error');
+//     } else {
+//       console.log("送信完了: " + info.response);
+//       res.send("成功しました");
+//     }
+//   })
+// })
 
 
 
